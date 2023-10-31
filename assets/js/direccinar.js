4a706769c5a18ccebb472ef2de7db7e1847acaca
Geraldine-Condori <evitachambi1996@gmail.com>
2023-10-31 07:44:18 -0400

    eva
    terminadoiiii

---
+++ assets/js/direccinar.js
@@ -0,0 +1,26 @@
+function usuario(){
+    location.href="/template/usuario/usuario.html"
+}
+function datos(){
+    location.href="/template/datos/datos.html"
+}
+function cliente(){
+    location.href="/template/cliente/clientes.html"
+}
+
+function empleados(){
+    location.href="/template/empleados/empleado.html"
+}
+
+function productos(){
+    location.href="/template/productos/productos.html"
+}
+function proveedor(){
+    location.href="/template/proveedor/proveedor.html"
+}
+function reportes(){
+    location.href="/template/reportes/reportes.html"
+}
+function ventas(){
+    location.href="/template/ventas/ventas.html"
+}
